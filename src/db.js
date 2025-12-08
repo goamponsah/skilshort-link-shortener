2b119bfc55950c723c6b71cc325483bf1f6c6101
goamponsah <ofosuamponsahgt@gmail.com>
2025-12-08 23:19:35 +0000

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,1 +1,16 @@
+// src/db.js
+const { Pool } = require("pg");
 
+const isProduction = process.env.NODE_ENV === "production";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction ? { rejectUnauthorized: false } : false
+});
+
+pool.on("error", (err) => {
+  console.error("Unexpected PG client error", err);
+  process.exit(-1);
+});
+
+module.exports = pool;
