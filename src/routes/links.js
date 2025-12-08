3e095528a1ce7aedb78e0c2eec6eb2271d724f8d
goamponsah <ofosuamponsahgt@gmail.com>
2025-12-08 23:21:08 +0000

    Update links.js

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,1 +1,19 @@
+// src/routes/links.js
+const express = require("express");
+const router = express.Router();
+const {
+  createShortLink,
+  redirectByCode,
+  getLinkStats
+} = require("../controllers/linksController");
 
+// API to create a new short link
+router.post("/api/shorten", createShortLink);
+
+// API to see stats of a short link
+router.get("/api/stats/:code", getLinkStats);
+
+// Redirect route (must be last so it doesn't catch /api paths)
+router.get("/:code", redirectByCode);
+
+module.exports = router;
